9ac23205ea033809ecfba21ade4a508a1f71207b
David Bauer <bauer@synyx.de>
2018-11-12 22:19:13 +0100

    jojojo

--- main.js
+++ main.js
@@ -18,28 +18,31 @@ var time = new Date();
 function getPrinter(host,args,name){
 	rclient.get('http://' + host + '/api/printer', args, function (data) {
 		// Bed
-		var tBed = {value: data.temperature.bed.actual, time: time};
-		var tBedTarget = {value: data.temperature.bed.target, time: time};
-		// Extruder
-		var tEx = {value: data.temperature.tool0.actual, time: time};
-		var tExTarget = {value: data.temperature.tool0.target, time: time};
+		console.log(data);
+		if(data != "Printer is not operational"){
+			var tBed = {value: data.temperature.bed.actual, time: time};
+			var tBedTarget = {value: data.temperature.bed.target, time: time};
+			// Extruder
+			var tEx = {value: data.temperature.tool0.actual, time: time};
+			var tExTarget = {value: data.temperature.tool0.target, time: time};
+			console.log(tBed);
 
-		client.writeMeasurement(
-			'temperature', [
-				{
-					fields: {
-						t_bed: tBed.value,
-						t_bed_target: tBedTarget.value,
-						t_ex: tEx.value,
-						t_ex_target: tExTarget.value
-					},
-					tags: { host: name },
-				}
-			]
-		).catch(err => {
-			console.error(`Error saving data to InfluxDB! ${err.stack}`)
-		})
-
+			client.writeMeasurement(
+				'temperature', [
+					{
+						fields: {
+							t_bed: tBed.value,
+							t_bed_target: tBedTarget.value,
+							t_ex: tEx.value,
+							t_ex_target: tExTarget.value
+						},
+						tags: { host: name },
+					}
+				]
+			).catch(err => {
+				console.error(`Error saving data to InfluxDB! ${err.stack}`)
+			})
+		}
 	}).on('error', function (err) {
 		console.log('something went wrong on the request', err.request.options);
 	});
@@ -49,50 +52,53 @@ function getPrinter(host,args,name){
 function getJob(host, args,name ){
 		rclient.get('http://' + host + '/api/job', args, function (data) {
 		// Status
-		var status = {value: data.state, time: time};
-		client.writeMeasurement(
-			'status', [
-				{
-					fields: {
-						status: status.value
-					},
-					tags: { host: name },
-				}
-			]
-		)
-
-		// Only update the job status while the printer is printing
-		if(data.state == "Printing" || data.state == "Paused") {
-			// Progress
-			var completion = {value: data.progress.completion, time: time};
-
-			// Print Time
-			var printTime = {value: data.progress.printTime, time: time};
-			var printTimeLeft = {value: data.progress.printTimeLeft, time: time};
-
+		console.log(data);
+		if(data != "Printer is not operational"){
+			var status = {value: data.state, time: time};
 			client.writeMeasurement(
-			'status', [
-				{
-					fields: {
-						completion: completion.value,
-						printTime: printTime.value,
-						printTimeLeft: printTimeLeft.value,
-					},
-					tags: { host: name },
-				}
-			])
-		}else{
-			client.writeMeasurement(
-			'status', [
-				{
-					fields: {
-						completion: 0,
-						printTime: 0,
-						printTimeLeft: 0,
-					},
-					tags: { host: name },
-				}
-			])
+				'status', [
+					{
+						fields: {
+							status: status.value
+						},
+						tags: { host: name },
+					}
+				]
+			)
+
+			// Only update the job status while the printer is printing
+			if(data.state == "Printing" || data.state == "Paused") {
+				// Progress
+				var completion = {value: data.progress.completion, time: time};
+
+				// Print Time
+				var printTime = {value: data.progress.printTime, time: time};
+				var printTimeLeft = {value: data.progress.printTimeLeft, time: time};
+
+				client.writeMeasurement(
+				'status', [
+					{
+						fields: {
+							completion: completion.value,
+							printTime: printTime.value,
+							printTimeLeft: printTimeLeft.value,
+						},
+						tags: { host: name },
+					}
+				])
+			}else{
+				client.writeMeasurement(
+				'status', [
+					{
+						fields: {
+							completion: 0,
+							printTime: 0,
+							printTimeLeft: 0,
+						},
+						tags: { host: name },
+					}
+				])
+			}
 		}
 	}).on('error', function (err) {
 		console.log('something went wrong on the request', err.request.options);
